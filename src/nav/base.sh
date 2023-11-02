d9241e6612842b38b233407c2c3a8c96d6ea46ce
Morris de Haan <morrisdh18@gmail.com>
2023-11-02 10:51:45 +0100

    base + rover rtk scripts

---
+++ src/nav/base.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This script takes the GPS input from USB port ttyS0
+#  and streams it through a tcp server at port 5050 (could be any available port).
+#  The rover can then use this as an input.
+str2str -in serial://ttyS0 -out tcpsvr://5050
---
+++ src/nav/rover.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Runs the RTK solution with the given settings.
+rtkrcv -o rtk.conf
